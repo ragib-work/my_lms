6bd277ae9d4c70bbf4ced1f206e41504e094a9c4
ragib-work <ragib.work@gmail.com>
2025-01-04 14:40:32 +0530

    Added whitenoise

--- src/academy/settings.py
+++ src/academy/settings.py
@@ -167,6 +167,7 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+WHITENOISE_SKIP_COMPRESS_EXTENSIONS = ['js.map', 'css.map']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
